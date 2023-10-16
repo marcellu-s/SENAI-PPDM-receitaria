e420c783be94dfcc5f71e0088c70a26d6cdf3241
marcellu-s <marcellus.costa@outlook.com>
2023-10-16 17:23:01 -0300

    FINISHED: Tela de login e cadastro

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,31 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
+
 import HomeScreen from '../views/Home';
 import RecipeScreen from '../views/Recipe';
+import LoginScreen from '../views/Login';
+import RegisterScreen from '../views/Register';
 
 const Stack = createNativeStackNavigator();
 
 const Routes = () => {
 
+    const [fontsLoaded] = useFonts({
+        'Poppins-Light': require('../assets/fonts/Poppins/Poppins-Light.ttf'),
+        'Poppins-Regular': require('../assets/fonts/Poppins/Poppins-Regular.ttf'),
+        'Poppins-Bold': require('../assets/fonts/Poppins/Poppins-Bold.ttf'),
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='Home' screenOptions={{ headerShown: false }}>
+            <Stack.Navigator initialRouteName='Login' screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Register" component={RegisterScreen} />
                 <Stack.Screen name="Home" component={HomeScreen} />
                 <Stack.Screen name="Recipe" component={RecipeScreen} />
             </Stack.Navigator>
